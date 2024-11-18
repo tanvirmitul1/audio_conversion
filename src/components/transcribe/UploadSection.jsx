bcde466e7da06133e460d9561471a77dd6423c6d
Tanvir Imam Mitul <mitulseopage1@gmail.com>
2024-11-18 22:23:25 +0600

    file drag and drop add

--- src/components/transcribe/UploadSection.jsx
+++ src/components/transcribe/UploadSection.jsx
@@ -1,19 +1,56 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import styled from "styled-components";
 import { FaUpload } from "react-icons/fa";
 import useColors from "../../hooks/useColors";
 
 const UploadSection = ({ handleFileUpload }) => {
+  const [isDragging, setIsDragging] = useState(false);
   const colors = useColors();
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setIsDragging(true);
+  };
+
+  const handleDragLeave = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setIsDragging(false);
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setIsDragging(false);
+
+    const file = e.dataTransfer.files[0]; // Access the first dropped file
+    if (file) {
+      handleFileUpload({ target: { files: [file] } }); // Mimic the input event structure
+    }
+  };
+
   return (
     <Card colors={colors}>
       <h3 style={{ color: colors?.text, marginBottom: "10px" }}>
         Upload Audio File
       </h3>
-      <UploadArea colors={colors}>
+      <UploadArea
+        colors={colors}
+        isDragging={isDragging}
+        onDragOver={handleDragOver}
+        onDragEnter={handleDragOver}
+        onDragLeave={handleDragLeave}
+        onDrop={handleDrop}
+      >
         <label htmlFor="file-upload">
           <FaUpload size={30} color={colors?.primary} />
-          <p>Drag and drop or click to upload an audio file</p>
+          <p>
+            {isDragging
+              ? "Release to upload"
+              : "Drag and drop or click to upload an audio file"}
+          </p>
         </label>
         <input
           id="file-upload"
@@ -49,13 +86,17 @@ const Card = styled.div`
 const UploadArea = styled.div`
   text-align: center;
   width: 100%;
-  border: 2px dashed ${({ colors }) => colors?.border};
+  border: 2px dashed
+    ${({ colors, isDragging }) =>
+      isDragging ? colors?.primary : colors?.border};
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 10px;
+  padding: 20px;
   border-radius: 10px;
   cursor: pointer;
+  background-color: ${({ isDragging }) =>
+    isDragging ? "rgba(0, 123, 255, 0.1)" : "transparent"};
 
   label {
     display: flex;
